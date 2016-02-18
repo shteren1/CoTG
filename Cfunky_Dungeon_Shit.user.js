b55578f3a931edcd45674acbaca98370b885f29b
shteren1 <shteren@gmail.com>
2016-02-18 19:47:49 +0200

    Update Cfunky_Dungeon_Shit.user.js

--- Cfunky_Dungeon_Shit.user.js
+++ Cfunky_Dungeon_Shit.user.js
@@ -10,7 +10,6 @@
 (function FunkyScript() {
 var loot=[0,400,1000,4500,15000,33000,60000,120000,201000,300000,446000];
     var numbs=[0,0,0];
-    var isopen=false;
 
     $(document).ready(function() {
         $("#loccavwarconGo").click(function() {
@@ -23,20 +22,20 @@ var loot=[0,400,1000,4500,15000,33000,60000,120000,201000,300000,446000];
         });
 
         $("#idluniwarconGo").click(function() {
-            if (!isopen)
-            { isopen=true;
-                createTable();}
+            createTable();
+
         });
 
         $("#raidDungGo").click(function() {
-            if (!isopen)
-            {isopen=true;
-                createTable();}
+            createTable();
         });
+
+
     });
 
     function createTable() {
-        var outtable="<div id='cfunkydiv' style='width:500px;height:330px;background-color: #E2CBAC;-moz-border-radius: 10px;-webkit-border-radius: 10px;border-radius: 10px;border: 4px ridge #DAA520;position:absolute;right:10px;top:100px; z-index:1000000;'><div class=\"popUpBar\"> <span class=\"ppspan\">Suggested Raiding Numbers</span> <button id=\"cfunkyX\" onclick=\"$('#cfunkydiv').remove();\";class=\"xbutton greenb\"><div id=\"xbuttondiv\"><div><div id=\"centxbuttondiv\"></div></div></div></button></div><div class=\"popUpWindow\">";
+        $('#cfunkydiv').remove();
+        var outtable="<div id='cfunkydiv' style='width:500px;height:330px;background-color: #E2CBAC;-moz-border-radius: 10px;-webkit-border-radius: 10px;border-radius: 10px;border: 4px ridge #DAA520;position:absolute;right:10px;top:100px; z-index:1000000;'><div class=\"popUpBar\"> <span class=\"ppspan\">Suggested Raiding Numbers</span> <button id=\"cfunkyX\" onclick=\"$('#cfunkydiv').remove();\" class=\"xbutton greenb\"><div id=\"xbuttondiv\"><div><div id=\"centxbuttondiv\"></div></div></div></button></div><div class=\"popUpWindow\">";
         outtable+="<table><thead><th>Lvl</th><th>Estimated Loot</th><th>Vanqs/Rangers<br>druids</th><th>Sorcs</th><th>Praetors</th><th>Arbs/Horses</th></thead>";
         outtable+="<tbody><tr><td>1</td><td>400</td><td>41</td><td>82</td><td>20</td><td>27</td></tr>";
         outtable+="<tr><td>2</td><td>1000</td><td>102</td><td>204</td><td>51</td><td>68</td></tr>";
@@ -52,7 +51,10 @@ var loot=[0,400,1000,4500,15000,33000,60000,120000,201000,300000,446000];
         outtable+="</div></div>";
         $( "body" ).append(outtable);
         $( "#cfunkydiv" ).draggable({ handle: ".popUpBar" , containment: "window", scroll: false});
+
     }
+
+
     function getDugRows() {
         $("#dungloctab tr").each(function() {
             var buttont=$(this).find( "button");
@@ -76,7 +78,7 @@ var loot=[0,400,1000,4500,15000,33000,60000,120000,201000,300000,446000];
             numbs[0]=Number(temp);
             temp=progress.match(/\d+/gi);
             numbs[1]=Number(temp);
-            //console.log(numbs[1]);
+            console.log(numbs[1]);
             setTimeout(function(){
 
                 $(".tninput").each(function() {
@@ -125,10 +127,12 @@ var loot=[0,400,1000,4500,15000,33000,60000,120000,201000,300000,446000];
                         //warships
                         numbs[2]=Math.ceil(loot[numbs[0]]*((100-numbs[1])*0.008+1)/3000);
                         $('#'+trpinpid).val(numbs[2]);
-                    } 
+                    }
+
                 });
+
             }, 300);
         });
     }
-});
 
+})();
